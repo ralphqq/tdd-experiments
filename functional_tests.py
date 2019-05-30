f77dc6236384333a0340a5328074a9264c0f25a8
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-05-31 05:03:51 +0800

    Refactored code for list table item checking

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, expected_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(expected_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User visits homepage
         self.browser.get('http://localhost:8000')
@@ -37,11 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         expected_list_item_1 = f'1: {list_item_1}'
-        self.assertIn(expected_list_item_1, [row.text for row in rows])
+        self.check_for_row_in_list_table(expected_list_item_1)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
@@ -52,11 +54,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         expected_list_item_2 = f'2: {list_item_2}'
-        self.assertIn(expected_list_item_1, [row.text for row in rows])
-        self.assertIn(expected_list_item_2, [row.text for row in rows])
+        self.check_for_row_in_list_table(expected_list_item_1)
+        self.check_for_row_in_list_table(expected_list_item_2)
 
         # User wonders whether the site will remember her list.
         # Then she sees the site has generated a unique URL for her
