21349b3e0459a7cc398bc3ea7dcf6af521abc29c
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-05-31 06:00:30 +0800

    Added Item model

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
 
@@ -16,3 +18,24 @@ class HomePageTest(TestCase):
         self.assertIn(submitted_text, response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        text_1 = 'The first item ever'
+        first_item.text = text_1
+        first_item.save()
+
+        second_item = Item()
+        text_2 = 'Item the second'
+        second_item.text = text_2
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, text_1)
+        self.assertEqual(second_saved_item.text, text_2)
