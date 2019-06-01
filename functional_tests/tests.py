f275b5c4d3b867e324ca8b496fe8a6c4f467fa11
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-06-01 21:55:31 +0800

    Replaced explicit wait with more reliable wait

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,11 @@ import time
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+
+MAX_WAIT = 10   # seconds
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -15,10 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
-    def check_for_row_in_list_table(self, expected_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(expected_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, expected_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(expected_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -44,9 +57,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
         expected_list_item_1 = f'1: {list_item_1}'
-        self.check_for_row_in_list_table(expected_list_item_1)
+        self.wait_for_row_in_list_table(expected_list_item_1)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
@@ -54,12 +66,11 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys(list_item_2)
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+        
         # The page updates again, and now shows both items on her list
         expected_list_item_2 = f'2: {list_item_2}'
-        self.check_for_row_in_list_table(expected_list_item_1)
-        self.check_for_row_in_list_table(expected_list_item_2)
+        self.wait_for_row_in_list_table(expected_list_item_1)
+        self.wait_for_row_in_list_table(expected_list_item_2)
 
         # User wonders whether the site will remember her list.
         # Then she sees the site has generated a unique URL for her
