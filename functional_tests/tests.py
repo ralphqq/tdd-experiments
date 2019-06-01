d10991f563ee37b512001c61990a8a1608cba0f7
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-06-01 21:19:27 +0800

    Turned functional_tests into an app

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,26 +1,29 @@
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, expected_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(expected_text, [row.text for row in rows])
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User visits homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # User sees the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -65,6 +68,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits that URL - her to-do list is still there.
 
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
