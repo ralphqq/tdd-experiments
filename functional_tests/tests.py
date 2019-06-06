fcc688e9d935c3de0205a3e953e1b6250c8a6aa6
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-06-07 06:25:21 +0800

    Added FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -131,3 +131,29 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn(user_1_item_1, page_text)
         self.assertIn(user_2_item_1, page_text)
+
+
+    def test_layout_and_styling(self):
+        # User goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # User notices the input box is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # User starts a list and
+        # notices        that input box is centered there, too
+        input_box.send_keys('Trying it out')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Trying it out')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
