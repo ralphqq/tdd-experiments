f4798c7f17a6cd088250a0777968593eb520829a
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-07-09 17:58:16 +0800

    Separated lists UTs into models and views tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,38 +10,6 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        test_list = List()
-        test_list.save()
-
-        first_item = Item()
-        text_1 = 'The first item ever'
-        first_item.text = text_1
-        first_item.parent_list = test_list
-        first_item.save()
-
-        second_item = Item()
-        text_2 = 'Item the second'
-        second_item.text = text_2
-        second_item.parent_list = test_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, test_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, text_1)
-        self.assertEqual(second_saved_item.text, text_2)
-        self.assertEqual(first_saved_item.parent_list, test_list)
-        self.assertEqual(second_saved_item.parent_list, test_list)
-
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
